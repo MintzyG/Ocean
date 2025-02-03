ddbab43a9d3a1cd2d1892622d7e2e73120f8dcc1
EmanuelPeixoto <peixoto_emanuel@hotmail.com>
2025-02-02 21:35:45 -0300

    Falling Sand Water working resize

--- FallingSandWater/main.c
+++ FallingSandWater/main.c
@@ -4,19 +4,11 @@
 #include <unistd.h>
 #include <curses.h>
 
-#ifndef SIZE_Y
-#define SIZE_Y LINES
-#endif
-
-#ifndef SIZE_X
-#define SIZE_X COLS
-#endif
-
-int** create() {
-  int** sandbox = calloc(SIZE_Y, sizeof *sandbox);
-  for (int i = 0; i < SIZE_Y; i++) {
-    sandbox[i] = calloc(SIZE_X, sizeof *(sandbox[0]));
-    for (int j = 0; j < SIZE_X; j++) {
+int** create(int size_x, int size_y) {
+  int** sandbox = calloc(size_y, sizeof *sandbox);
+  for (int i = 0; i < size_y; i++) {
+    sandbox[i] = calloc(size_x, sizeof *(sandbox[0]));
+    for (int j = 0; j < size_x; j++) {
       sandbox[i][j] = !(rand() % 5);
     }
   }
@@ -24,19 +16,19 @@ int** create() {
   return sandbox;
 }
 
-void print(int** sandbox) {
-  for (int i = 0; i < SIZE_Y; i++) {
-    for (int j = 0; j < SIZE_X; j++) {
+void print(int** sandbox, int size_x, int size_y) {
+  for (int i = 0; i < size_y; i++) {
+    for (int j = 0; j < size_x; j++) {
       printw("%c", sandbox[i][j] ? '@' : '.');
     }
     move(i, 0);
   }
 }
 
-int proccess(int** sandbox) {
+int proccess(int** sandbox, int size_x, int size_y) {
   int moved = 0, tmp = 0;
-  for (int i = SIZE_Y - 1; i >= 0; i--) {
-    for (int j = SIZE_X - 1; j >= 0; j--) {
+  for (int i = size_y - 1; i >= 0; i--) {
+    for (int j = size_x - 1; j >= 0; j--) {
       // Move Down
       if (i > 0) {
         if (sandbox[i][j] == 0 && sandbox[i-1][j] == 1) {
@@ -60,7 +52,7 @@ int proccess(int** sandbox) {
       }
 
       // Slide Right
-      if (j != SIZE_X - 1 && i > 0) {
+      if (j != size_x - 1 && i > 0) {
         if (sandbox[i][j] == 1 && sandbox[i][j+1] == 0 && sandbox[i-1][j] == 1) {
           tmp = sandbox[i-1][j];
           sandbox[i-1][j] = sandbox[i][j+1];
@@ -84,7 +76,7 @@ int proccess(int** sandbox) {
         }
       } else {
         // Move Right
-        if (j != SIZE_X - 1) {
+        if (j != size_x - 1) {
           if (sandbox[i][j] == 1 && sandbox[i][j+1] == 0) {
             tmp = sandbox[i][j];
             sandbox[i][j] = sandbox[i][j+1];
@@ -100,6 +92,7 @@ int proccess(int** sandbox) {
 }
 
 int main() {
+  int size_x, size_y;
   char str;
 
   initscr();
@@ -107,10 +100,11 @@ int main() {
 
   srand(time(NULL));
   while (str != 'q'){
-    int **sandbox = create();
+    getmaxyx(stdscr, size_y, size_x);
+    int **sandbox = create(size_x, size_y);
     str = ' ';
-    while (proccess(sandbox) && str != 'q' && str != 'r') {
-      print(sandbox);
+    while (proccess(sandbox, size_x, size_y) && str != 'q' && str != 'r') {
+      print(sandbox, size_x, size_y);
       str = getch();
       refresh();
       clear();
