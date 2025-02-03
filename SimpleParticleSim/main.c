685d6176b03bd32c2195e72ce801d48cd87b311b
MintzyG <ericbraga2000@gmail.com>
2025-02-02 22:51:14 -0300

    Applied restitution

--- SimpleParticleSim/main.c
+++ SimpleParticleSim/main.c
@@ -97,22 +97,21 @@ void ApplyGravity(Shape *s) {
 }
 
 void RenderLoop(Scene s) {
-  bool colorSwitch = true;
   int i = 0, color = 22;
   float angle = 0.0f, radius = 10.0f;
   int centerX = s.sBuff[0].posX;
   int centerY = s.sBuff[0].posY;
-  while (i++ < 100) {
+  while (i++ < 1000) {
     Render(s, color);
     s.sBuff[0].Gravity(&s.sBuff[0]);
-    s.sBuff[0].posX += s.sBuff[0].vX;
-    s.sBuff[0].posY += s.sBuff[0].vY;
     if (s.sBuff[0].posX + s.sBuff[0].radius > s.c.width || s.sBuff[0].posX - s.sBuff[0].radius < 0) {
-      s.sBuff[0].vX = -s.sBuff[0].vX;
+      s.sBuff[0].vX = (int)(-s.sBuff[0].vX * 0.9);
     }
     if (s.sBuff[0].posY + s.sBuff[0].radius > s.c.height || s.sBuff[0].posY - s.sBuff[0].radius < 0) {
-      s.sBuff[0].vY = -s.sBuff[0].vY;
+      s.sBuff[0].vY = (int)(-s.sBuff[0].vY * 0.9);
     }
+    s.sBuff[0].posX += s.sBuff[0].vX;
+    s.sBuff[0].posY += s.sBuff[0].vY;
     usleep(100 * 1000);
     printf("\e[1:1H\e[2J");
   }
@@ -130,7 +129,7 @@ int main() {
   }
 
   Camera c = { .x = 1, .y = 0, .z = 0, .height = w.ws_row, .width = w.ws_col, .is_2d = true, .PixelBuffer = CreatePixelBuffer(w.ws_row, w.ws_col) };
-  Shape circle = { .posX = w.ws_col/2, .posY = w.ws_row/2, .vX = 10, .vY = 0, .type = Circle, .radius = 10, .Gravity = ApplyGravity };
+  Shape circle = { .posX = w.ws_col/2, .posY = w.ws_row/2, .vX = 10, .vY = -1, .type = Circle, .radius = 10, .Gravity = ApplyGravity };
   Shape *sBuff = calloc(12, sizeof *sBuff);
   sBuff[0] = circle;
   Scene s = { .c = c, .sBuff = sBuff };
